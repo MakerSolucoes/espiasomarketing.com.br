77032542f23af06ded08c30efd3c1ac48e46dc78
Andre Freire <andrefreirecg@gmail.com>
2023-04-08 19:15:51 -0300

    ajust_logger

--- wp-content/themes/espiaso/assets/js/logger.js
+++ wp-content/themes/espiaso/assets/js/logger.js
@@ -2,17 +2,28 @@ function checkAuth() {
   const atual_url = window.location.href;
   const username = "espia";
   const password = "espia@2023";
-  const promptText = "Digite seu usuário e senha no padrão usuario:senha";
-  console.log(atual_url)
-  if (atual_url !== "https://espiasomarketing.com.br.loc") {
-    return;
-  }else{
-    const credentials = prompt(promptText);
-    if (credentials !== `${username}:${password}`) {
-      alert("Usuário ou senha inválidos!");
+
+  const cookies = document.cookie.split("; ");
+  const loginCookie = cookies.find(cookie => cookie.startsWith("login="));
+  if (loginCookie) {
+    const [, credentials] = loginCookie.split("="); 
+    if (credentials === `${username}:${password}`) {
+      console.log("Usuário autenticado!");
+      return;
     }
   }
+
+  const promptText = "Digite seu usuário e senha no padrão usuario:senha";
+  const credentials = prompt(promptText);
+  if (credentials !== `${username}:${password}`) {
+    alert("Usuário ou senha inválidos!");
+    checkAuth();
+  } else {
+    const date = new Date();
+    date.setTime(date.getTime() + 12 * 60 * 60 * 1000); // 12 horas em milissegundos
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = `login=${credentials};${expires};path=/`;
+  }
 }
-  
+
 checkAuth();
-  
